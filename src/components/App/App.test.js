1a2ead22e09e0b39f263e6532f66a3ce6ef5c777
gktnkrdg <gktnkrdg@gmail.com>
2020-09-04 03:15:20 +0300

    Filename change

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 let container
-
 beforeEach(() => (container = shallow(<App />)))
 it('renders without crashing', () => {
   shallow(<App />);
